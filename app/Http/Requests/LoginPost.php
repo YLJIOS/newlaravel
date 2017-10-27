6c51f8de1439c9603bd7cdcd4f0b87d598c6c6a8
sunny <2722179982@qq,com>
2017-10-27 16:17:49 +0800

    lll

---
+++ app/Http/Requests/LoginPost.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginPost extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'username'=>'required|string|min:5|max:20',
+            'password'=>'required|string|min:5',
+        ];
+    }
+
+    /**
+     * 获取已定义的验证规则的错误消息。
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'username.required' => 'The :attribute field is 必须的.',
+            'password.required'  => 'A message is required',
+        ];
+    }
+}
